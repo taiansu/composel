7d2e90f3d1c7d8a7ede13e19337a3a3879428f31
taiansu <taiansu@gmail.com>
2019-12-03 18:13:12 +0800

    Accept curried function with arguments

--- lib/composel.rb
+++ lib/composel.rb
@@ -13,8 +13,21 @@ module Composel
 
   def composition(lambdas, operator)
     lambdas.reverse.reduce do |l, accu|
-      lmbd = l.is_a?(Array) ? l[0].method(l[1]) : l
+      lmbd = to_lambda(l)
       accu.method(operator).call(lmbd)
     end
+  end
+
+  def to_lambda(maybe_proc)
+    return maybe_proc if maybe_proc.is_a?(Proc)
+
+    sym_or_obj, fun, args = Array(maybe_proc)
+
+    unless sym_or_obj
+      raise ArgumentError.new("Accept a proc/lambda, a symbol or an array contains [object, :method_symbol, [args]]")
+    end
+
+    lmbd = fun ? sym_or_obj.method(fun) : method(sym_or_obj)
+    args ? lmbd.curry.call(*args) : lmbd
   end
 end
--- test/composel_test.rb
+++ test/composel_test.rb
@@ -1,13 +1,21 @@
 require_relative "./test_helper"
 
 class Klass
-  def add_ten(i)
-    i + 10
+  def add_ten(num)
+    num + 10
+  end
+
+  def add_all(num1, num2, num3)
+    num1 + num2 + num3
   end
 end
 
 class ComposelTest < Minitest::Test
   include Composel
+
+  def instance_multiply_three(num)
+    num * 3
+  end
 
   def test_compose_lambdas
     f = ->(x) { x + 1 }
@@ -47,6 +55,55 @@ class ComposelTest < Minitest::Test
     i = pipe(f, g, [o, :add_ten])
 
     assert_equal(i.call(2), (f >> g >> o.method(:add_ten)).call(2))
+  end
+
+  def test_compose_curried_method
+    f = ->(x) { x + 1 }
+    g = ->(x) { x * 2 }
+    o = Klass.new
+
+    i = compose(
+          f,
+          g,
+          [o, :add_all, [2, 3]]
+        )
+
+    assert_equal(i.call(10),
+                 (f << g << o.method(:add_all).curry.call(2, 3)).call(10))
+  end
+
+  def test_pipe_curried_method
+    f = ->(x) { x + 1 }
+    g = ->(x) { x * 2 }
+    o = Klass.new
+
+    i = pipe(
+          f,
+          g,
+          [o, :add_all, [10, 11]]
+        )
+
+    assert_equal(i.call(10),
+                 (f >> g >> o.method(:add_all).curry.call(10, 11)).call(10))
+  end
+
+  def test_compose_symbol
+    f = ->(x) { x + 1 }
+    g = ->(x) { x * 2 }
+
+    i = compose(f, g, :instance_multiply_three)
+
+    assert_equal(i.call(10),
+                 (f << g << method(:instance_multiply_three)).call(10))
+  end
+
+  def test_method_not_defined
+    f = ->(x) { x + 1 }
+    g = ->(x) { x * 2 }
+
+    assert_raises NameError do
+      compose(f, g, :undefined_method)
+    end
   end
 
   def test_that_it_has_a_version_number
